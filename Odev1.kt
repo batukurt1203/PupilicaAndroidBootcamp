4dd78e0efee454776509255a90004a89d3c7bea2
Batuhan Kurt <124192682+batukurt1203@users.noreply.github.com>
2025-04-30 01:40:12 +0300

    Create Odev1.kt

---
+++ Odev1.kt
@@ -0,0 +1,48 @@
+ fun main(){
+        var şehir="İstanbul"
+        var ülke="Türkiye"
+        var telefon="05055554321"
+        var postaKodu="34320"
+        var email="abc@gmail.com"
+        var meslek="Doktor"
+        var stokMiktarı=18
+        var müşteriAdı="Ali Yılmaz"
+        var bakiye=1870
+        var doğumGünü="06/06/1999"
+        var maaş=55000
+        var medeniDurum="Bekar"
+        var ürünYorum="Ürün gayet kaliteli"
+
+        var ödemeTarihi="31/12/2025"
+        var ödeme=450
+        var siparişAdeti=100
+        var arabaModeli="Renault i20"
+        var yayınlanmaTarihi="01/01/2024"
+        var indirimMiktarı=0.30
+        var odaSayısı=5
+        var enlem=42.15
+        var boylam=26.78
+        var ürünAdı="Polo Yaka Tişört"
+        var yemekFiyatı=400
+
+        var marka="Microsoft"
+        var müzikAdı="Bir Derdim Var"
+        var videoSüresi=121.24
+        var ürünPuanı=4.32
+        var resimAdı="dog.png"
+        var dosyaFormatı="jpg"
+        var renk="kırmızı"
+        var renkKodu="baacc7"
+        var telefonModeli="Iphone 16"
+        var ağırlık=46
+
+        var ulusalGün="23 Nisan"
+        var tatilGünü="24 Ocak"
+        var rezervasyonTarihi="12/04/2025"
+        var sokakAdı="87228."
+        var otobüsHattı="KM45"
+        var kalanDakika=27
+        var takipKodu="486257"
+        var kuponSüresi=87
+        var kuponKodu="1786"
+        var faturaAdresi="Ali Cad. Veli Mah. Ahmet Apt Kat:3 No:10"
